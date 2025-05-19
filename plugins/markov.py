cfd5cb3dc082f61165c3b4ef931ed7b17ac8debf
djinzi <maidnaut@gmail.com>
2025-05-18 23:25:36 -0700

    added profanity filter and filesize limit

--- plugins/markov.py
+++ plugins/markov.py
@@ -1,6 +1,7 @@
 import discord, asyncio, os, re, random
 from collections import defaultdict
 from discord.ext import commands
+from profanity_check import predict, predict_prob
 
 def setup(bot):
     bot.add_cog(MarkovCog(bot))
@@ -9,18 +10,21 @@ class MarkovCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.chains = defaultdict(lambda: defaultdict(list))
+        self.key_order = defaultdict(deque)
         self.data_dir = "inc/markov"
+        self.MAX_ENTRIES = 50000
         
         os.makedirs(self.data_dir, exist_ok=True)
         self.load_all_chains()
 
-        # Autosave
-        self.save_interval = 300  # 5 minutes
+        self.save_interval = 300 # 5 minute autosave
         self.bg_task = self.bot.loop.create_task(self.auto_save())
 
+    # Get the filepath
     def get_chain_path(self, guild_id):
         return os.path.join(self.data_dir, f"{guild_id}.txt")
 
+    # Autosave
     async def auto_save(self):
 
         await self.bot.wait_until_ready()
@@ -32,13 +36,30 @@ class MarkovCog(commands.Cog):
                 path = self.get_chain_path(guild_id)
                 try:
                     with open(path, "w", encoding="utf-8") as f:
-                        for key, values in self.chains[guild_id].items():
+                        for key in list(self.key_order[guild_id])[-self.MAX_ENTRIES:]:
+                            values = self.chains[guild_id].get(key, [])
                             line = f"{' '.join(key)}|{','.join(values)}\n"
                             f.write(line)
-
                 except Exception as e:
                     print(f"Error saving chain for guild {guild_id}: {e}")
 
+    # Load everything at startup
+    def load_all_chains(self):
+
+        try:
+            for filename in os.listdir(self.data_dir):
+                if filename.endswith(".txt"):
+
+                    try:
+                        guild_id = int(filename[:-4])
+                        self.load_chain(guild_id)
+                    except (ValueError, IndexError):
+                        continue
+                        
+        except FileNotFoundError:
+            pass
+
+    # Load the guild's specific chain
     def load_chain(self, guild_id):
         path = self.get_chain_path(guild_id)
         if not os.path.exists(path):
@@ -55,27 +76,16 @@ class MarkovCog(commands.Cog):
                         key_part, values_part = line.split("|", 1)
                         key = tuple(key_part.split())
                         values = values_part.split(",")
-                        self.chains[guild_id][key] = values
+                        
+                        if len(self.key_order[guild_id]) < self.MAX_ENTRIES:
+                            self.chains[guild_id][key] = values
+                            self.key_order[guild_id].append(key)
+                        else:
+                            break
                     except ValueError:
                         continue
-
         except Exception as e:
             print(f"Error loading chain for guild {guild_id}: {e}")
-
-    def load_all_chains(self):
-
-        try:
-            for filename in os.listdir(self.data_dir):
-                if filename.endswith(".txt"):
-
-                    try:
-                        guild_id = int(filename[:-4])
-                        self.load_chain(guild_id)
-                    except (ValueError, IndexError):
-                        continue
-                        
-        except FileNotFoundError:
-            pass
 
 
 
@@ -84,8 +94,8 @@ class MarkovCog(commands.Cog):
     async def train_markov(self, message):
         if not message.guild:
             return
-            
-        # Remove all mentions
+        
+        # Clean out the mentions
         clean_content = re.sub(r'<@!?\d+>', '', message.content)
         clean_content = re.sub(r'<@&\d+>', '', clean_content)
         clean_content = re.sub(r'<#\d+>', '', clean_content)
@@ -94,6 +104,10 @@ class MarkovCog(commands.Cog):
         if not clean_content:
             return
 
+        if predict([clean_content])[0] == 1:
+            return
+
+
         guild_id = message.guild.id
         words = clean_content.split()
         
@@ -101,7 +115,18 @@ class MarkovCog(commands.Cog):
             return
             
         for i in range(len(words) - 2):
-            self.chains[guild_id][(words[i], words[i+1])].append(words[i+2])
+            key = (words[i], words[i+1])
+            self.chains[guild_id][key].append(words[i+2])
+            
+            # Maintain the max filesize
+            if key not in self.key_order[guild_id]:
+                self.key_order[guild_id].append(key)
+                
+                # Bump out old entries
+                while len(self.key_order[guild_id]) > self.MAX_ENTRIES:
+                    oldest_key = self.key_order[guild_id].popleft()
+                    if oldest_key in self.chains[guild_id]:
+                        del self.chains[guild_id][oldest_key]
 
 
 
